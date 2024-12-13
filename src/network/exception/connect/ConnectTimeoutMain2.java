0d22087713964519f5bf59684878b34d65341cf7
onepunchkim <hyukjin4711@gmail.com>
2024-12-13 17:28:49 +0900

    network study

---
+++ src/network/exception/connect/ConnectTimeoutMain2.java
@@ -0,0 +1,17 @@
+package network.exception.connect;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class ConnectTimeoutMain2 {
+    public static void main(String[] args) throws IOException {
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress("192.168.1.250", 45678), 1000);
+        } catch (ConnectException e) {
+            e.printStackTrace();
+        }
+    }
+}
