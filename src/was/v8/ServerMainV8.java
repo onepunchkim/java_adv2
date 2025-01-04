c7d52e415ae6c6cbf1a4871bc87586450d485616
onepunchkim <hyukjin4711@gmail.com>
2025-01-04 18:12:35 +0900

    http annotation study

--- src/was/v8/ServerMainV8.java
+++ src/was/v8/ServerMainV8.java
@@ -5,6 +5,7 @@ import was.httpserver.ServletManager;
 import was.httpserver.servlet.DiscardServlet;
 import was.httpserver.servlet.HttpServer;
 import was.httpserver.servlet.annotation.AnnotationServletV2;
+import was.httpserver.servlet.annotation.AnnotationServletV3;
 
 import java.io.IOException;
 import java.util.List;
@@ -14,7 +15,8 @@ public class ServerMainV8 {
 
     public static void main(String[] args) throws IOException {
         List<Object> controllers = List.of(new SiteControllerV8(), new SearchControllerV8());
-        HttpServlet servlet = new AnnotationServletV2(controllers);
+        //HttpServlet servlet = new AnnotationServletV2(controllers);
+        HttpServlet servlet = new AnnotationServletV3(controllers);
 
         ServletManager servletManager = new ServletManager();
         servletManager.setDefaultServlet(servlet);
